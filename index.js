8f2c47e73d2c35149e309c5dca9537ce0770772d
SFrasia <108404961+SFrasia@users.noreply.github.com>
2024-01-16 21:49:00 +0300

    Changes

---
+++ index.js
@@ -0,0 +1,10 @@
+// Write your code here!
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+const newHeader = document.createElement("h1").remove();
+newHeader.innerHTML = "YOUR-NAME is the champion";
